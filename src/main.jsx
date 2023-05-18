bff5850d89a49addd52fd3be9159d3c06bbb02ad
shazidkarim <shazid747@gmail.com>
2023-05-18 17:33:15 +0600

    json file created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
